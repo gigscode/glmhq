6fa6673d52d73892111721c930281aca96daaf02
gigscode <gigsdev007@gmail.com>
2025-11-25 10:31:06 +0100

    feat: add Auxano Centers page displaying center details, schedules, and location information.

--- app/auxano-centers/page.tsx
+++ app/auxano-centers/page.tsx
@@ -16,7 +16,7 @@ export default function AuxanoCentersPage() {
         {
             name: "FEDERAL POLYTECHNIC ADO-EKITI (MAIN CAMPUS)",
             location: "A2-005, Old SBS Building, Federal Polytechnic Main Campus, Ado-Ekiti, Nigeria",
-            schedule: "Sundays, 4PM || Thursdays: 5PM",
+            schedule: "Sundays: 4PM || Thursdays: ",
             time: "5PM",
             image: fpadoImage
         },
@@ -124,15 +124,20 @@ export default function AuxanoCentersPage() {
                                     {/* Schedule */}
                                     <div className="flex items-start gap-2 mb-4">
                                         <svg
-                                            className="w-5 h-5 text-red-600 flex-shrink-0 mt-0.5"
-                                            fill="currentColor"
-                                            viewBox="0 0 20 20"
+                                            className="w-5 h-5 flex-shrink-0 mt-0.5"
+                                            width="24"
+                                            height="24"
+                                            viewBox="0 0 24 24"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
                                         >
-                                            <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd" />
+                                            <path fillRule="evenodd" clipRule="evenodd" d="M4 4.018C3.46 4.041 3.072 4.092 2.729 4.208C2.14238 4.40442 1.60932 4.73422 1.17171 5.17148C0.734106 5.60874 0.403882 6.14154 0.207 6.728C1.49012e-08 7.349 0 8.115 0 9.649C0 9.744 1.11759e-08 9.792 0.013 9.83C0.0252768 9.86679 0.0459435 9.90021 0.0733657 9.92763C0.100788 9.95506 0.134214 9.97572 0.171 9.988C0.209 10.001 0.257 10.001 0.353 10.001H17.647C17.743 10.001 17.791 10.001 17.829 9.988C17.8658 9.97572 17.8992 9.95506 17.9266 9.92763C17.9541 9.90021 17.9747 9.86679 17.987 9.83C18 9.791 18 9.743 18 9.647C18 8.114 18 7.347 17.793 6.729C17.5964 6.14217 17.2663 5.60897 16.8287 5.17135C16.391 4.73372 15.8578 4.40362 15.271 4.207C14.928 4.092 14.539 4.041 14 4.018V6.5C14 6.89782 13.842 7.27936 13.5607 7.56066C13.2794 7.84196 12.8978 8 12.5 8C12.1022 8 11.7206 7.84196 11.4393 7.56066C11.158 7.27936 11 6.89782 11 6.5V4H7V6.5C7 6.89782 6.84196 7.27936 6.56066 7.56066C6.27936 7.84196 5.89782 8 5.5 8C5.10218 8 4.72064 7.84196 4.43934 7.56066C4.15804 7.27936 4 6.89782 4 6.5V4.018Z" fill="#D00002" />
+                                            <path d="M0 11.5C0 11.264 -4.47035e-08 11.146 0.073 11.073C0.146 11 0.264 11 0.5 11H17.5C17.736 11 17.854 11 17.927 11.073C18 11.146 18 11.264 18 11.5V12C18 15.771 18 17.657 16.828 18.828C15.656 19.999 13.771 20 10 20H8C4.229 20 2.343 20 1.172 18.828C0.000999928 17.656 0 15.771 0 12V11.5Z" fill="#F36D6D" fillOpacity="0.4" />
+                                            <path d="M5.5 2.5V6.5M12.5 2.5V6.5" stroke="#D00002" strokeLinecap="round" />
                                         </svg>
                                         <p className="text-sm text-gray-700 leading-snug">
                                             <span className="font-semibold">{center.schedule}</span>
-                                            {center.time && <span className="ml-1">{center.time}</span>}
+                                            {center.time && <span className="ml-1 text-[#D00002] font-semibold">{center.time}</span>}
                                         </p>
                                     </div>
 
